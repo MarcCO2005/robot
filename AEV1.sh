48f507718af23d2269acc9be274d10253fc0a1af
MarcCO2005 <macuol@floridauniversitaria.es>
2024-01-31 06:19:12 -0500

    Añadiendo voz

--- AEV1.sh
+++ AEV1.sh
@@ -6,3 +6,5 @@ URL="https://www.periodismo.com/2024/01/22/nuevo-record-guinness-un-hombre-tomo-
 
 wget -O archivo.html $URL 
 
+cat archivo.html | grep -o '<title>.*</title>' | sed 's/\///g' | sed 's/<title>//g' >> titular.txt
+espeak -f titular.txt
